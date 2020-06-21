a978fd070638442303384e8b1cee95f272504894
Daniel Hahm <dhahm01@gmail.com>
2020-06-21 16:05:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,9 +11,13 @@ songs = [
 ]
 
 
-def help()
+def help
   #output instructions for user on how to use the jukebox
-  
+  p 'I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program'
 end
 
 def play()
